7e9d7aff7136fa01d39e10c0039c22ec1b108308
DestinyOMGwz <destinyomgwz@gmail.com>
2015-03-23 20:15:51 -0400

    ex 12.2

---
+++ ch12/StrBlob.cpp
@@ -0,0 +1,58 @@
+#include "StrBlob.h"
+
+// Constructor
+StrBlob::StrBlob() : data(std::make_shared<std::vector<std::string>>()) {}
+
+StrBlob::StrBlob(const std::initializer_list<std::string> &il) : data(std::make_shared<std::vector<std::string>>(il)) {}
+
+void StrBlob::check(StrBlob::size_type i, const std::string &msg) const
+{
+	if (i >= data->size()) throw std::out_of_range(msg);
+}
+
+// Accessing function
+StrBlob::size_type StrBlob::size() const
+{
+	return data->size();
+}
+
+bool StrBlob::empty() const
+{
+	return data->empty();
+}
+
+std::string& StrBlob::front()
+{
+	check(0, "front on empty StrBlob");
+	return data->front();
+}
+
+std::string& StrBlob::back()
+{
+	check(0, "back on empty StrBlob");
+	return data->back();
+}
+
+const std::string& StrBlob::front() const
+{
+	check(0, "front on empty StrBlob");
+	return data->front();
+}
+
+const std::string& StrBlob::back() const
+{
+	check(0, "back on empty StrBlob");
+	return data->back();
+}
+
+void StrBlob::push_back(const std::string &t)
+{
+	data->push_back(t);
+}
+
+void StrBlob::pop_back()
+{
+	check(0, "pop_back on empty StrBlob");
+	data->pop_back();
+}
+
---
+++ ch12/StrBlob.h
@@ -0,0 +1,42 @@
+// 
+// StrBlob.h
+// Exercise 12.1
+// 
+// Created by DestinyOMGwz on 3/23/15
+// Copyright (c) 2015 DestinyOMGwz. All rights reserved.
+//
+// Write your own version of the StrBlob class including the const versions of front and back.
+
+#ifndef _STRBLOB_H_
+#define _STRBLOB_H_
+#include <memory>
+#include <string>
+#include <vector>
+
+class StrBlob
+{
+public:
+	typedef std::vector<std::string>::size_type size_type;
+	// Constructors
+	StrBlob();
+	StrBlob(const std::initializer_list<std::string> &il);
+	virtual ~StrBlob();
+
+	// Accessing functions
+	size_type size() const;
+	bool empty() const;
+	std::string& front();
+	std::string& back();
+	const std::string& front() const;
+	const std::string& back() const;
+
+	// Functionality
+	void push_back(const std::string &t);
+	void pop_back();
+
+private:
+	std::shared_ptr<std::vector<std::string>> data;
+	void check(size_type i, const std::string &msg) const;
+};
+
+#endif
